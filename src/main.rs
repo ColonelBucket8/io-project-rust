ed76df55ba3747f342a9025c742d1a6323170b11
Aimi Fadhil <freedomterran89@gmail.com>
2022-10-25 10:52:17 +0800

    added accepting command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    // collect is one function you do often need to 
+    // annotate because Rust isn't able to infer the kind
+    // of collection you want
+    let args: Vec<String> = env::args().collect();
+
+    let query = &args[1];
+    let file_path = &args[2];
+
+    println!("Searching for {}", query);
+    println!("In file {}", file_path);
+    dbg!("query: {}, file path: {}", query, file_path);
 }
