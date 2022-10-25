f24936595489203762a7e0ceb65d9bba8a994453
Aimi Fadhil <freedomterran89@gmail.com>
2022-10-25 10:59:06 +0800

    added reading a file

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use std::env;
+use std::fs;
 
 fn main() {
-    // collect is one function you do often need to 
+    // collect is one function you do often need to
     // annotate because Rust isn't able to infer the kind
     // of collection you want
     let args: Vec<String> = env::args().collect();
@@ -12,4 +13,8 @@ fn main() {
     println!("Searching for {}", query);
     println!("In file {}", file_path);
     dbg!("query: {}, file path: {}", query, file_path);
+
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+
+    println!("With text:\n{contents}");
 }
