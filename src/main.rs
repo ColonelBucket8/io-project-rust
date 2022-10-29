0eacca606c9be30b7f0cca3bbacef6cb302e3efa
ColonelBucket8 <59568628+ColonelBucket8@users.noreply.github.com>
2022-10-30 01:53:43 +0800

    changed to iterator fp style

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,7 @@ fn main() {
     // collect is one function you do often need to
     // annotate because Rust isn't able to infer the kind
     // of collection you want
-    let args: Vec<String> = env::args().collect();
-
-    let config = Config::build(&args).unwrap_or_else(|err| {
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
